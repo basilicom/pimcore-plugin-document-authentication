11a7e0cb838c5a5db7a2365401a8f694945c0be0
Marco Senkpiel <marco.senkpiel@basilicom.de>
2016-03-17 13:26:12 +0100

    updated php namespaces and compatibility to pimcore 4

--- lib/DocumentAuthentication/FrontControllerPlugin.php
+++ lib/DocumentAuthentication/FrontControllerPlugin.php
@@ -1,94 +1,100 @@
 <?php
 
-class DocumentAuthentication_FrontControllerPlugin extends Zend_Controller_Plugin_Abstract
-{
+    namespace DocumentAuthentication;
 
-    public function  preDispatch(Zend_Controller_Request_Abstract $request)
+    use Pimcore\Model\Document\Page;
+    use Pimcore\Model\User;
+    use Pimcore\Tool\Authentication;
+    use Pimcore\Tool\Frontend;
+
+    class FrontControllerPlugin extends \Zend_Controller_Plugin_Abstract
     {
-        parent::preDispatch($request);
 
-        if ($request->getParam("document") instanceof Document_Page) {
+        public function preDispatch(\Zend_Controller_Request_Abstract $request)
+        {
+            parent::preDispatch($request);
 
-            $this->handleDocumentAuthentication($request->getParam("document"));
+            if ($request->getParam("document") instanceof Page) {
+
+                $this->handleDocumentAuthentication($request->getParam("document"));
+            }
+
         }
 
-    }
+        /**
+         * @param Page $document
+         */
+        private function handleDocumentAuthentication($document)
+        {
+            if (is_object($document)) {
 
-    /**
-     * @param $document Pimcore_Document
-     * @return void
-     */
-    private function handleDocumentAuthentication($document)
-    {
-        if (is_object($document)) {
+                if (!$document->getProperty(Plugin::DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED)) {
+                    return; // all OK, show page
+                }
+            }
 
-            if (!$document->getProperty(DocumentAuthentication_Plugin::DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED)) {
+            $user = Authentication::authenticateSession();
+            if ($user instanceof User) {
                 return; // all OK, show page
             }
+
+            if (self::isDocumentAuthenticationValid()) {
+                return; // all OK, show page
+            }
+
+            $this->sendHttpBasicAuthResponse();
+            exit;
         }
 
-        $user = Pimcore_Tool_Authentication::authenticateSession();
-        if ($user instanceof User) {
-            return; // all OK, show page
-        }
+        /**
+         * @return bool
+         */
+        private function isDocumentAuthenticationValid()
+        {
 
-        if (self::isDocumentAuthenticationValid()) {
-            return; // all OK, show page
-        }
+            $config = Frontend::getWebsiteConfig();
 
-        $this->sendHttpBasicAuthResponse();
-        exit;
-    }
+            $username = $config->get(Plugin::CONFIG_DOCUMENT_AUTHENTICATION_USERNAME, 'preview');
+            $password = $config->get(Plugin::CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD, '');
 
-    /**
-     * @return bool
-     */
-    private function isDocumentAuthenticationValid()
-    {
+            if (trim($password) == '') {
+                // empty password - this is not good; Deny access!
+                return false;
+            }
 
-        $config = Pimcore_Tool_Frontend::getWebsiteConfig();
+            if (($_SERVER['PHP_AUTH_USER'] === $username) && ($_SERVER['PHP_AUTH_PW'] === $password)) {
+                return true;
+            }
 
-        $username = $config->get(DocumentAuthentication_Plugin::CONFIG_DOCUMENT_AUTHENTICATION_USERNAME, 'preview');
-        $password = $config->get(DocumentAuthentication_Plugin::CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD, '');
-
-        if (trim($password) == '') {
-            // empty password - this is not good; Deny access!
             return false;
         }
 
-        if (($_SERVER['PHP_AUTH_USER'] === $username) && ($_SERVER['PHP_AUTH_PW'] === $password)) {
-            return true;
+        private function sendHttpBasicAuthResponse()
+        {
+            $config = Frontend::getWebsiteConfig();
+            $password = $config->get(Plugin::CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD, null);
+
+            if (($password === null) || (trim($password) == '')) {
+
+                $notice = 'Missing or empty Website Property '
+                    . Plugin::CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD;
+
+            } else {
+
+                $notice = 'Authentication required';
+            }
+
+            /** @var $response \Zend_Controller_Response_Http */
+            $response = $this->getResponse();
+
+            $response->setHeader('Cache-Control', 'max-age=0');
+            $response->setHttpResponseCode(401);
+            $response->setHeader(
+                'WWW-Authenticate',
+                'Basic realm="' . $notice . '"'
+            );
+
+            $response->setBody('Unauthorized.');
+            $response->sendResponse();
         }
-
-        return false;
     }
-
-    private function sendHttpBasicAuthResponse()
-    {
-        $config = Pimcore_Tool_Frontend::getWebsiteConfig();
-        $password = $config->get(DocumentAuthentication_Plugin::CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD, null);
-
-        if (($password === null) || (trim($password) == '')) {
-
-            $notice = 'Missing or empty Website Property '
-                . DocumentAuthentication_Plugin::CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD;
-
-        } else {
-
-            $notice = 'Authentication required';
-        }
-
-        /** @var $response Zend_Controller_Response_Http */
-        $response = $this->getResponse();
-
-        $response->setHeader('Cache-Control', 'max-age=0');
-        $response->setHttpResponseCode(401);
-        $response->setHeader(
-            'WWW-Authenticate',
-            'Basic realm="' . $notice . '"'
-        );
-
-        $response->setBody('Unauthorized.');
-        $response->sendResponse();
-    }
-}
--- lib/DocumentAuthentication/Plugin.php
+++ lib/DocumentAuthentication/Plugin.php
@@ -1,100 +1,90 @@
 <?php
 
-class DocumentAuthentication_Plugin
-    extends Pimcore_API_Plugin_Abstract
-    implements Pimcore_API_Plugin_Interface
-{
+    namespace DocumentAuthentication;
 
-    const DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED
-        = 'documentAuthenticationEnabled';
+    use Pimcore\API\Plugin as PluginApi;
+    use Pimcore\Db;
+    use Pimcore\Model\Property\Predefined as PropertyPredefined;
 
-    const CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD
-        = 'documentAuthenticationPassword';
-
-    const CONFIG_DOCUMENT_AUTHENTICATION_USERNAME
-        = 'documentAuthenticationUser';
-
-    const DB_TABLE_WEBSITE_SETTINGS
-        = 'website_settings';
-
-    const DB_TABLE_PRE_PROPERTIES
-        = 'properties_predefined';
-
-    public function init()
-    {
-        Pimcore::getEventManager()->attach("system.startup", function ($event) {
-
-            $front = Zend_Controller_Front::getInstance();
-
-            $frontControllerPlugin = new DocumentAuthentication_FrontControllerPlugin();
-            $front->registerPlugin($frontControllerPlugin);
-        });
-    }
-
-    public static function install()
+    class Plugin extends PluginApi\AbstractPlugin implements PluginApi\PluginInterface
     {
 
-        $database = Pimcore_Resource_Mysql::getConnection();
+        const DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED
+            = 'documentAuthenticationEnabled';
 
-        if (!self::isInstalled()) {
+        const CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD
+            = 'documentAuthenticationPassword';
 
-            $database->insert(self::DB_TABLE_PRE_PROPERTIES, array(
-                'name' => self::DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED,
-                'key' => self::DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED,
-                'type' => 'bool',
-                'inheritable' => 1,
-                'ctype' => 'document'
-            ));
+        const CONFIG_DOCUMENT_AUTHENTICATION_USERNAME
+            = 'documentAuthenticationUser';
 
-            $database->insert(self::DB_TABLE_WEBSITE_SETTINGS, array(
-                'name' => self::CONFIG_DOCUMENT_AUTHENTICATION_USERNAME,
-                'type' => 'text',
-                'data' => 'preview'
-            ));
+        const DB_TABLE_WEBSITE_SETTINGS
+            = 'website_settings';
 
-            $database->insert(self::DB_TABLE_WEBSITE_SETTINGS, array(
-                'name' => self::CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD,
-                'type' => 'text',
-                'data' => md5(uniqid('', true))
-            ));
+        public function init()
+        {
+            \Pimcore::getEventManager()->attach("system.startup", function ($event) {
+
+                $front = \Zend_Controller_Front::getInstance();
+
+                $frontControllerPlugin = new FrontControllerPlugin();
+                $front->registerPlugin($frontControllerPlugin);
+            });
         }
 
-        return 'Successfully installed plugin DocumentAuthentication.';
+        public static function install()
+        {
+            $database = Db::get();
+
+            if (!self::isInstalled()) {
+
+                $prop = new PropertyPredefined();
+                $prop->setName(self::DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED);
+                $prop->setKey(self::DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED);
+                $prop->setType('bool');
+                $prop->setInheritable(1);
+                $prop->setCtype('document');
+                $prop->save();
+
+                $database->insert(self::DB_TABLE_WEBSITE_SETTINGS, array(
+                    'name' => self::CONFIG_DOCUMENT_AUTHENTICATION_USERNAME,
+                    'type' => 'text',
+                    'data' => 'preview'
+                ));
+
+                $database->insert(self::DB_TABLE_WEBSITE_SETTINGS, array(
+                    'name' => self::CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD,
+                    'type' => 'text',
+                    'data' => md5(uniqid('', true))
+                ));
+            }
+
+            return 'Successfully installed plugin DocumentAuthentication.';
+        }
+
+        public static function uninstall()
+        {
+            $database = Db::get();
+
+            $prop = PropertyPredefined::getByKey(self::DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED);
+            $prop->delete();
+
+            $sqlQuery = "DELETE FROM " . self::DB_TABLE_WEBSITE_SETTINGS . " WHERE name = ?";
+            $database->query($sqlQuery, array(self::CONFIG_DOCUMENT_AUTHENTICATION_USERNAME));
+            $database->query($sqlQuery, array(self::CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD));
+
+            return 'Successfully removed plugin DocumentAuthentication.';
+        }
+
+        public static function isInstalled()
+        {
+            return (PropertyPredefined::getByKey(self::DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED)
+                != null);
+        }
+
+        public static function needsReloadAfterInstall()
+        {
+            return false; // backend only functionality!
+        }
+
     }
-
-    public static function uninstall()
-    {
-        $database = Pimcore_Resource_Mysql::getConnection();
-
-        $sqlQuery = "DELETE FROM " . self::DB_TABLE_PRE_PROPERTIES . " WHERE name = ?";
-        $database->query($sqlQuery, array(self::DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED));
-
-        $sqlQuery = "DELETE FROM ".self::DB_TABLE_WEBSITE_SETTINGS." WHERE name = ?";
-        $database->query($sqlQuery, array(self::CONFIG_DOCUMENT_AUTHENTICATION_USERNAME));
-        $database->query($sqlQuery, array(self::CONFIG_DOCUMENT_AUTHENTICATION_PASSWORD));
-
-        return 'Successfully removed plugin DocumentAuthentication.';
-    }
-
-    public static function isInstalled()
-    {
-        $database = Pimcore_Resource_Mysql::getConnection();
-
-        $sqlQuery= "SELECT COUNT(id) as num FROM " . self::DB_TABLE_PRE_PROPERTIES . " WHERE name = ?";
-        $isInstalled = (
-            (int)$database->fetchOne(
-                $sqlQuery,
-                array(
-                    self::DOC_PROPERTY_DOCUMENT_AUTHENTICATION_ENABLED
-                )
-            ) > 0);
-
-        return $isInstalled;
-    }
-
-    public static function needsReloadAfterInstall()
-    {
-        return false; // backend only functionality!
-    }
-
-}
